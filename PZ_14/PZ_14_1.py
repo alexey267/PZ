d5c35857d2333e05cd6fd0008e5cb68ddff556e8
User861 <1029384756102938a4851029384756@gmail.com>
2024-03-15 13:18:10 +0300

    Done PZ №14

---
+++ PZ_14/PZ_14_1.py
@@ -0,0 +1,25 @@
+# Из исходного текстового файла (experience.txt) выбрать стаж работы. Посчитать
+# количество полученных элементов.
+
+import re
+import chardet
+
+with open('experience.txt', 'rb') as f:  # подбираем формат
+  rawdata = f.read()
+  result = chardet.detect(rawdata)
+  encoding = result['encoding']
+
+with open('experience.txt', encoding=encoding) as f:
+  lines = f.readlines()
+
+
+data = [match.group() for line in lines  # поиск совпадений
+        for match in [re.search(r'(\d+ \w+)(\s+\d+ \w+)?$', line)]
+        if match]
+
+
+print(data)  # вывод списка
+print(f'Количество сегментов: {len(data)}')  # количество сегментов списка
+
+
+
